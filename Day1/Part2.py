9faab1e9ca8c11818e3649e24ccee1a8801c888b
Omar Morales Rivera <102254539+ascyphermoonshot@users.noreply.github.com>
2023-12-01 14:09:44 -0600

    Update Part2.py
    fixed bug that didnt recognize overlapping matches

--- Day1/Part2.py
+++ Day1/Part2.py
@@ -1,27 +1,27 @@
 import regex as re
 
- with open('/storage/emulated/0/Download/input1.txt') as f:
-     calibs=f.read().splitlines()
+with open('input1.txt') as f:
+    calibs=f.read().splitlines()
 
- digit_dict = {
-     #'zero': 0,
-     'one': 1,
-     'two': 2,
-     'three': 3,
-     'four': 4,
-     'five': 5,
-     'six': 6,
-     'seven': 7,
-     'eight': 8,
-     'nine': 9
- }
+digit_dict = {
+    #'zero': 0,
+    'one': "o1ne",
+    'two': "t2wo",
+    'three': "t3hree",
+    'four': "f4our",
+    'five': "f5ive",
+    'six': "s6ix",
+    'seven': "s7even",
+    'eight': "e8ight",
+    'nine': "n9ine"
+}
 
- calsum=0
- for calib in calibs:
-     spelled_digits=(re.findall(r'(?:                                        one|two|three|four|five|six|seven|eight|nine)',calib))
-     for dig in spelled_digits:
-         calib=calib.replace(dig,str(digit_dict[dig]))
-     print(calib)
-     rcalib=calib[::-1]
-     calsum+=int(re.search(r"\d",calib).group()+re.search(r"\d",rcalib).     group())
- print(calsum)
+calsum=0
+for calib in calibs:
+    spelled_digits=(re.findall(r'(?:one|two|three|four|five|six|seven|eight|nine)',calib,overlapped=True))
+    for dig in spelled_digits:
+        calib=calib.replace(dig,str(digit_dict[dig]))
+    print(calib)
+    rcalib=calib[::-1]
+    calsum+=int(re.search(r"\d",calib).group()+re.search(r"\d",rcalib).group())
+print(calsum)
